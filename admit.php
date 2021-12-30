5eaddf400e872c3242db6ca49249964185d12efa
Md Mohiuddin <sobujmohiuddin1426@gmail.com>
2021-12-30 22:49:50 +0600

    updated

---
+++ admit.php
@@ -0,0 +1,148 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Development Area</title>
+	<!-- ALL CSS FILES  -->
+  <link rel="stylesheet" href="assets/css/all.min.css">
+  <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+<body >
+	
+    <section class="header">
+       <nav class="navbar  navbar-expand-lg navbar-light bg-black">
+         <div class="container">
+           <div class="col-md-6 col-12">
+            <a class="navbar-brand" href="#"><img style="width:70%"src="media/logo.png"></a>
+            </div>
+            <div class="col-md-6 col-12">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ml-auto mt-1">
+                 <li class="nav-item">
+                    <a class="nav-link nav2" href="index.php">Home</a>
+                 </li>
+                <li class="nav-item">
+                    <a class="nav-link nav1" href="admission.php">Admission</a>
+                </li>
+            
+                 <li class="nav-item">
+                    <a class="nav-link nav2" href="about_us.php">About us</a>
+                 </li>
+                <li class="nav-item">
+                    <a class="nav-link nav2" href="contact_us.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav2" href="career.php">Career</a>
+                 </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle nav2" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Login
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item log" href="adminlogin.php">Admin Login</a>
+                    <a class="dropdown-item log" href="studentlogin.php">Student Login</a>
+                    <a class="dropdown-item log" href="teacherlogin.php">Teacher Login</a>
+                    </div>
+                </li>
+              </ul>
+           </div>
+           </div>
+       </div>
+    </nav>
+
+   </section>
+  <section class="admission mt-5">
+      <div class="container">
+          <h3 class="text-center" style="color:#8B6700;font-weight:600">ADMISSION INFORMATION</h3>
+          <h4 style="color:white">Fee <span style="color:red">Structure</span></h4>
+          <h6 style="color:white">[ Session : [July 2021-June 2022]</h6>
+          <div class="row">
+              <div class="col-md-8 offset-2">
+               <table class="table mt-5">
+               <thead>
+                        <tr>
+                        
+                        <th>Class</th>
+                        <th>Amount</th>
+                       
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 1</td>
+                        <td style="color:#0CA116">9,870/-</td>
+                        </tr>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 2</td>
+                        <td style="color:#0CA116">9,870/-</td>
+                        </tr>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 3</td>
+                        <td style="color:#0CA116">10,017/-</td>
+                        </tr>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 4</td>
+                        <td style="color:#0CA116">10,017/-</td>
+                        </tr>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 5</td>
+                        <td style="color:#0CA116">10,310/-</td>
+                        </tr>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 6</td>
+                        <td style="color:#0CA116">10,310/-</td>
+                        </tr>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 7</td>
+                        <td style="color:#0CA116">11,041/-</td>
+                        </tr>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 8</td>
+                        <td style="color:#0CA116" >11,041/-</td>
+                        </tr>
+                        <tr style="color:white" class="table-row">
+                        <td>Class 9</td>
+                        <td style="color:#0CA116" >11,041/-</td>
+                        </tr>
+                        
+                   </tbody>
+               </table>
+              </div>
+          </div>
+      </div>
+  </section>
+  <section class="feeinfo"style="margin-top:600px">
+      <div class="container">
+           <h3 style="color:white">Fee <span style="color:red">Information<span></h3>
+           <ul class="list-unstyled">
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;Monthly Fees should be paid by the 10th of the Current Month.</li>
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;Last Date to pay Monthly Fees is on the 15th of the Current Month.</li>
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;If the 15th falls on a Scheduled Holiday [Weekly or Public] ,then the fees should be paid BEFORE that date.</li>
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;If FAILED to pay the Tuition Fees by the15th of the Current Month, a FINE of Taka 100/- per week will be applicable as LATE FEE.</li>
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;FEES COLLECTION TIME: 8:00 am to 3:00 pm.</li>
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;Session Fees for the Next Academic Session must be paid by the given schedule.</li>
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;Amount once paid will not be refundable.</li>
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;Report card will not be issued if fees are in arrears.</li>
+          <li style="color:white"><i class="fas fa-caret-right"></i>&nbsp;&nbsp;&nbsp;Contact Details: Tel # 02 47115628 , Online inquiry.</li>
+       
+
+      </ul>
+      </div>
+  </section>
+
+
+	<!-- JS FILES  -->
+  <script src="assets/js/script.js"></script>
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
